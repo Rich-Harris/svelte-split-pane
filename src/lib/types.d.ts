d436023e633f817c5fd2f5835dfc0d886cc8eb62
Rich Harris <rich.harris@vercel.com>
2024-12-16 17:30:55 -0500

    export types

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -2,3 +2,5 @@
  * A length expressed as `${number}px`, `${number}%`, `${number}em` or `${number}rem`
  */
 export type Length = `${number}px` | `${number}%` | `${number}em` | `${number}rem`;
+
+export * from './index';
